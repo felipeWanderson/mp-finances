ae597928c0ede9faa0f92f691a4695ed480a9475
Felipe Leal <felipewanderson2020@gmail.com>
2023-07-22 17:21:43 -0300

    feat: card

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,40 @@
+import styles from '@/components/common/Card/Card.module.css'
+import IncommeSVG from '@/assets/income.svg'
+import OutcommeSVG from '@/assets/outcome.svg'
+import TotalSVG from '@/assets/total.svg'
+import Image from 'next/image';
+
+interface CardProps {
+  titleCard: string;
+  amount: number;
+  upadateAt?: string;
+  type: 'INCOME' | 'OUTCOME' | 'TOTAL';
+}
+
+const ICONCARD = {
+  INCOME: IncommeSVG,
+  OUTCOME: OutcommeSVG,
+  TOTAL: TotalSVG
+}
+
+export function Card({ titleCard, type, amount, upadateAt }: CardProps) {
+
+  const amountFomatted = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(amount || 0)
+
+  const cardContainerClass = type !== 'TOTAL' ? styles.cardContainer : styles.cardContainerTotal
+  const cardAmountClass = type !== 'TOTAL' ? styles.cardAmount : styles.cardAmountTotal
+  const cardHeaderClass = type !== 'TOTAL' ? styles.cardHeader : styles.cardHeaderTotal
+
+  return (
+    <div className={cardContainerClass}>
+      <header className={cardHeaderClass}>
+        <span>{titleCard}</span>
+        <Image src={ICONCARD[type]} alt='' />
+      </header>
+      <div className={styles.cardContent}>
+        <p className={cardAmountClass}>{amountFomatted}</p>
+        {upadateAt && <span className={styles.lastUpdate}>{upadateAt}</span>}
+      </div>
+    </div>
+  )
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@/components/Header'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
+import { Card } from '@/components/common/Card';
 
 export default function Dashboard() {
   const { data: session } = useSession();
@@ -20,6 +21,13 @@ export default function Dashboard() {
   return (
     <main>
       <Header />
+      <section className={styles.dashboardContent}>
+        <div className={styles.cardsContainer}>
+          <Card titleCard='Entradas' amount={17400.00} type='INCOME'/>
+          <Card titleCard='Saídas' amount={1259.00} type='OUTCOME' />
+          <Card titleCard='Total' amount={16141.00} type='TOTAL'/>
+        </div>
+      </section>
     </main>
   )
 
